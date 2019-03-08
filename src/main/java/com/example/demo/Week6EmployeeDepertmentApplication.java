ba6449cc41be4ed030e3fb0e37fd994cb349b3d5
TOyefule <Tolani.Oyefule@gmail.com>
2019-03-08 10:08:39 -0500

    INitialized Week 6 CHallenge

---
+++ src/main/java/com/example/demo/Week6EmployeeDepertmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week6EmployeeDepertmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week6EmployeeDepertmentApplication.class, args);
+    }
+
+}
